91cd237b7921756eb6178d0b7e3703f4b943c7d7
Anand Capur <arcdigital@users.noreply.github.com>
2017-05-08 09:52:36 -0700

    Fixes for PyPI and bump version

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ except(IOError, ImportError):
 
 setup(
     name='tangocard-raasv2',
-    version='1.0.0b0',
+    version='1.0.0b2',
     description='With this RESTful API you can integrate a global reward or incentive program into your app or platform. If you have any questions or if you\'d like to receive your own credentials, please contact us at devsupport@tangocard.com.',
     long_description=long_description,
     author='Tango Card, Inc.',
     author_email='devsupport@tangocard.com',
     url='https://github.com/tangocard/raas-v2-sdk-python',
-    download_url='https://github.com/tangocard/raas-v2-sdk-python/archive/1.0.0b0.tar.gz',
+    download_url='https://github.com/tangocard/raas-v2-sdk-python/archive/1.0.0b2.tar.gz',
     packages=find_packages(),
     install_requires=[
         'requests>=2.9.1, <3.0',
