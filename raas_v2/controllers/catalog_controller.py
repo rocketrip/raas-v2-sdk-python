1fe3f9c0760ccb180b43c0932d15e40d0840489a
Jason Unger <jason@rocketrip.com>
2017-05-17 14:08:22 -0400

    Allow verbose to be passed into get_catalog

--- raas_v2/controllers/catalog_controller.py
+++ raas_v2/controllers/catalog_controller.py
@@ -21,13 +21,23 @@ class CatalogController(BaseController):
         super(CatalogController, self).__init__(client, call_back)
         self.logger = logging.getLogger(__name__)
 
-    def get_catalog(self):
+    def get_catalog(self, options=dict()):
         """Does a GET request to /catalogs.
 
         Get Catalog
 
+        Args:
+            options (dict, optional): Key-value pairs for any of the
+                parameters to this API Endpoint. All parameters to the
+                endpoint are supplied through the dictionary with their names
+                being the key and their desired values being the value. A list
+                of parameters that can be used are::
+
+                    verbose -- bool -- TODO: type description
+                        here.
+
         Returns:
-            CatalogModel: Response from the API. 
+            CatalogModel: Response from the API.
 
         Raises:
             APIException: When an error occurs while fetching the data from
@@ -38,26 +48,29 @@ class CatalogController(BaseController):
         """
         try:
             self.logger.info('get_catalog called.')
-    
+
             # Prepare query URL
             self.logger.info('Preparing query URL for get_catalog.')
             _query_builder = Configuration.get_base_uri()
             _query_builder += '/catalogs'
             _query_url = APIHelper.clean_url(_query_builder)
-    
+            _query_parameters = {
+                'verbose': options.get('verbose', True),
+            }
+
             # Prepare headers
             self.logger.info('Preparing headers for get_catalog.')
             _headers = {
                 'accept': 'application/json'
             }
-    
+
             # Prepare and execute request
             self.logger.info('Preparing and executing request for get_catalog.')
-            _request = self.http_client.get(_query_url, headers=_headers)
+            _request = self.http_client.get(_query_url, headers=_headers, query_parameters=_query_parameters)
             BasicAuth.apply(_request)
             _context = self.execute_request(_request, name = 'get_catalog')
             self.validate_response(_context)
-    
+
             # Return appropriate type
             return APIHelper.json_deserialize(_context.response.raw_body, CatalogModel.from_dictionary)
 
